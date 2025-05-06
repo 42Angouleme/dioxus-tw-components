12963d65f5956746a63bc11596d49c027f9f2e65
kbz_8 <kbz_8.dev@akel-engine.com>
2025-05-06 14:00:49 +0200

    Update props.rs

--- src/components/molecules/pagination/props.rs
+++ src/components/molecules/pagination/props.rs
@@ -94,7 +94,7 @@ pub fn Pagination(mut props: PaginationProps) -> Element {
     });
 
     rsx! {
-        div { class: "flex flex-row gap-2 justify-center items-center",
+        div { tw_merge!("flex flex-row gap-2 justify-center items-center", props.class.read()),
             Button {
                 class: tw_merge!("flex justify-center items-center", props.class.read()),
                 style: props.style,
