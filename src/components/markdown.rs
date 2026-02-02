f986ac1ef30d0a51838164ccfd20828f8538efb8
Kbz-8 <kbz_8.code@proton.me>
2026-02-02 10:30:39 +0100

    fixing formatting

--- src/components/markdown.rs
+++ src/components/markdown.rs
@@ -34,7 +34,7 @@ pub fn Markdown(props: MarkdownProps) -> Element {
 /// assert_eq!(html, "<h1>Hello World</h1>");
 /// ```
 fn string_to_html(content: String) -> String {
-    use pulldown_cmark::{html, Options, Parser};
+    use pulldown_cmark::{Options, Parser, html};
 
     let mut options = Options::empty();
     options.insert(Options::ENABLE_TABLES);
