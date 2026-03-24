324f091018016cfb55486640dd84b9a8b87e2e78
kneshi <gaetan.randria@hotmail.com>
2026-03-24 20:57:33 +0100

    feat: add loading prop to Button component (spinner + disabled)

--- src/components/button.rs
+++ src/components/button.rs
@@ -1,5 +1,7 @@
 use dioxus::prelude::*;
 
+
+
 #[derive(Clone, PartialEq, Props)]
 pub struct ButtonProps {
     /// Additional attributes to apply to the element
@@ -23,6 +25,10 @@ pub struct ButtonProps {
     #[props(default = false)]
     noclasses: bool,
 
+    /// Show loading state: spinner + "Loading..." text, button disabled
+    #[props(default = false)]
+    loading: bool,
+
     /// The children element
     children: Element,
 }
@@ -32,19 +38,49 @@ pub fn Button(mut props: ButtonProps) -> Element {
     let default_classes = "button";
     crate::setup_class_attribute(&mut props.attributes, default_classes);
 
-    let onclick = move |event| props.onclick.call(event);
-    let ondoubleclick = move |event| props.ondoubleclick.call(event);
-    let onmousedown = move |event| props.onmousedown.call(event);
-    let onmouseup = move |event| props.onmouseup.call(event);
+    let loading = props.loading;
+    let onclick = move |event| {
+        if !loading {
+            props.onclick.call(event);
+        }
+    };
+    let ondoubleclick = move |event| {
+        if !loading {
+            props.ondoubleclick.call(event);
+        }
+    };
+    let onmousedown = move |event| {
+        if !loading {
+            props.onmousedown.call(event);
+        }
+    };
+    let onmouseup = move |event| {
+        if !loading {
+            props.onmouseup.call(event);
+        }
+    };
 
     rsx! {
         button {
+            disabled: loading,
             onclick,
             ondoubleclick,
             onmousedown,
             onmouseup,
             ..props.attributes,
-            {props.children}
+            if loading {
+                style { "@keyframes btn-spin {{ from {{ transform: rotate(0deg); }} to {{ transform: rotate(360deg); }} }}" }
+                span {
+                    class: "inline-flex items-center gap-2 opacity-70 pointer-events-none",
+                    span {
+                        style: "font-family: 'Material Symbols Rounded'; font-size: 1.2em; line-height: 1; animation: btn-spin 1s linear infinite; display: inline-block;",
+                        "progress_activity"
+                    }
+                    "Loading..."
+                }
+            } else {
+                {props.children}
+            }
         }
     }
 }
